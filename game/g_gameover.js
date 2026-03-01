98d27b9b100d505c0586b920a8628f636372c924
Roksana Strzelecki <110071030+roxyyfoxyy@users.noreply.github.com>
2026-03-01 23:49:58 +0100

    Updates

--- game/g_gameover.js
+++ game/g_gameover.js
@@ -12,16 +12,11 @@ function triggerGameOver() {
     gameState.gameOver = true;
 
 
-    // GAMEOVER-VISUAL (derzeit noch Popup, später Window!!!)
-    const popup = document.querySelector(".popup-text");
-    popup.textContent = "GAME OVER";
-
-    // Animation-Reset (für wenn vorher schon aktiviert)
-    popup.classList.remove("animation");
-    void popup.offsetWidth; // Reflow Trick (damit Browser remove auch wirklich erkennt!)
-
-    // Animation-Start
-    popup.classList.add("animation");
+    // GAMEOVER-WINDOW
+    window.parent.postMessage(
+        { type: "GAME_OVER" },
+        "*"
+    );
 
 
     // Buttons deaktivieren
