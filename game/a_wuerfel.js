294fed2aa6f758b8e8d7597af0a5dd74ac1c16c5
Roksana Strzelecki <110071030+roxyyfoxyy@users.noreply.github.com>
2026-03-25 21:04:31 +0100

    Updates

--- game/a_wuerfel.js
+++ game/a_wuerfel.js
@@ -10,7 +10,15 @@
 	let locked = [false, false, false, false, false];
 
 	// Platzhalter zur Speicherung des aktuellen gewürfelten Werts
-	let werte = [1, 1, 1, 1, 1];
+	//let werte = [1, 1, 1, 1, 1];
+	let werte = [null, null, null, null, null];
+
+
+	let freezeAktiv = false;
+	let frozenIndex = null;
+	let freezeEnemyId = null;
+	let firstRollDone = false;
+	let frozenValue = null;
 
 
 
@@ -25,6 +33,8 @@
 		// 1. Lösche vorigen Inhalt/Augen
 		wuerfelElement.innerHTML = "";
 	
+		// wegen FREEZE??
+		if (!value || value < 1 || value > 6) return;
 
 		// 2. Wert zuordnen zu Position auge (a1, a2, Kombis etc.)
 		// Wenn später Zahl, dann macht hier lookie-lookie, welche Augen dazugehören
@@ -56,15 +66,58 @@
 
 	// Würfeln/randomizing nur für not-locked-ones
 	function wuerfeln() {
+
+		firstRollDone = true;
 	
 		for (let i = 0; i < 5; i++) {										//gehe alle Würfel 0-4 durch im Array locked
-			if (!locked[i]) {												//wenn bei locked nicht auf true, dann:
+			
+			if (i === frozenIndex && freezeAktiv) {
+				frozenValue = werte[i];
+			}
+			else if (!locked[i]) {											//wenn bei locked nicht auf true, dann:
 				werte[i] = Math.floor(Math.random() * 6) + 1;				//random Abrundwert zwischen 0...5 +1 -> 1...6
 			}
 
+		
 			// hole entsprechenden Würfel
 			const w = document.getElementById("wuerfel" + (i + 1));			//+1 wegen 1-6 für ID statt 0-5
-			designWuerfel(w, werte[i]);										//Damit ab zu: Erstellung Design jeweiliger Würfel
+													
+			if (werte[i] !== null) {
+				designWuerfel(w, werte[i]);									//Damit ab zu: Erstellung Design jeweiliger Würfel
+			} 
+
+			if (i === frozenIndex && firstRollDone) {
+				w.classList.add("frozen");
+			} else {
+				w.classList.remove("frozen");
+			}
+
+
+		}
+		
+	}
+
+
+
+//************* FUNKTION: updateWuerfelAnzeige() *************//
+//*** direkt FREEZE wenn spawn bzw. UNFREEZE wenn besiegt ***//
+//***********************************************************//
+
+	function updateWuerfelAnzeige() {
+		for (let i = 0; i < 5; i++) {
+			const w = document.getElementById("wuerfel" + (i + 1));
+
+			// Würfel neu zeichnen (falls Wert vorhanden)
+			if (werte[i] !== null) {
+				designWuerfel(w, werte[i]);
+			}
+
+			// Frozen-Status korrekt setzen
+			if (i === frozenIndex && freezeAktiv) {
+				w.classList.add("frozen");
+			} else {
+				w.classList.remove("frozen");
+			}
 		}
 	}
 
@@ -78,6 +131,8 @@
 
 	function toggleLock(index) {											//index = welcher Würfel gerade angeklickt wurde
 
+		// ZWISCHEN: für Special-Gegner-FREEZE
+		if (index === frozenIndex) return;
 	
 		// 1. Invertieren: wenn dieser Angeklickte auf locked-true, dann jetzt false/frei 
 		// und wenn auf locked-false/frei, dann jetzt true
