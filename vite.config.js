8c3cca6cce5db0ced5843a8105f624bb376ff7f4
Roksana Strzelecki <110071030+roxyyfoxyy@users.noreply.github.com>
2026-04-07 17:04:20 +0200

    _v34

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/dice-wip/",
+  base: "/roll-and-conquer/",
   plugins: [react()],
 })
